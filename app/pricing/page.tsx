654ac54f11bf69a5853ff5f4cf3de17f09f16651
kjh0209 <kevin070209@gmail.com>
2026-02-20 18:03:14 +0900

    error modified

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useSession } from "next-auth/react";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import { getDictionary, Language, getInitialLanguage } from "@/lib/i18n";
 
